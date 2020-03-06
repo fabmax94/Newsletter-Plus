bd61a995639afa09b265f415cf36c7203e85acee
fabio.silva <fabio.silva@samsung.com>
2020-03-06 10:30:55 -0400

    fix migration

--- newsletter_plus/settings.py
+++ newsletter_plus/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'newsletter_plus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
 }
 
